d3f43781241c9ff0cfd40b199c93b586f356c26e
tristhaus <tristhaus@yahoo.de>
2026-02-03 19:50:01 +0100

    Create a basic placeholder
    Placeholder based on npm create vite@6.2.0

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React POC</h1>
+      <div>This is a placeholder for the eventual page.</div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
